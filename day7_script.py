f5490085133c13efbf71d99db3796c12bebea6f6
AYUSH-KOTHARIAK <ayushkothari1907@gmail.com>
2023-05-03 17:30:08 +0530

    first commit

---
+++ day7_script.py
@@ -0,0 +1,4 @@
+import my_file1
+##import new1.py  ## importing file present in difference folder
+newpack.new1.xyz() 
+my_file1.abc()
---
+++ newpack/__init__.py
@@ -0,0 +1,5 @@
+def pqr():
+    print('pqr')
+     
+
+# by making __init__.py file we are converting the folder into a library 
